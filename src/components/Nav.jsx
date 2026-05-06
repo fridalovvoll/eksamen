90ce72c85c2d2bb7c1cde8baf1ff0a544efc32c0
Aurorapedersenn <ape223@uit.no>
2026-05-06 17:02:30 +0200

    la inn design

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default function Nav() {
   return (
     <nav className="navbar">
-      <Link to="/hjem">Hjem</Link>
+      <Link to="/">Hjem</Link>
       <Link to="/utesteder">Utesteder</Link>
       <Link to="/hvorerfu">Hvor er FU</Link>
     </nav>
--- src/pages/Hjem.jsx
+++ src/pages/Hjem.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function Hjem() {
   return (
-    <div>
-      hei fra hjem
+    <div className="hero">
+      <div className="hero-content">
+        <h1 className="logo-main">Finn ditt neste</h1>
+        <h2 className="logo-script">utested</h2>
+        <p className="subtitle">Det er her det skjer!</p>
+      </div>
     </div>
   )
 }
