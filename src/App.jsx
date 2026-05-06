822971d2598fe4fee1940d42d5adeaf9af10e879
fridalovvoll <flo017@uit.no>
2026-05-06 11:24:23 +0200

    satt opp flere sider til nettsiden

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
+import { Routes, Route } from "react-router-dom";
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Hjem from "./pages/Hjem";
+import Utesteder from "./pages/Utesteder";
+import Nav from './components/Nav';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-dfgsfghshggdsh
+    <Nav/>
+
+    
+<Routes>
+      <Route path="/" element={<Hjem />} />
+      <Route path="/about" element={<Utesteder />} />
+    </Routes>
 
         </>
   )
---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div>
+      hei fra nav
+    </div>
+  )
+}
