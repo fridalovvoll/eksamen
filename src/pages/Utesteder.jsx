6dd8acbcf6b41225305725cb532020282568fcae
aly036 <aly036@uit.no>
2026-05-27 12:05:40 +0200

    endring på utesteder

--- src/pages/Utesteder.jsx
+++ src/pages/Utesteder.jsx
@@ -27,13 +27,10 @@ export default function Utesteder() {
   const musikkTyper = [...new Set(steder.flatMap((sted) => sted.Musikk || []))];
 
   const filtrerteSteder = steder.filter((sted) => {
-   const filtrerteSteder = steder.filter((sted) => {
-  const passerBy = valgtBy === "" || sted.By === valgtBy;
-  const passerVibe = valgtVibe === "" || sted.Vibe?.includes(valgtVibe);
-  const passerMusikk = valgtMusikk === "" || sted.Musikk?.includes(valgtMusikk);
-
-  return passerBy && passerVibe && passerMusikk;
-});
+    const passerBy = valgtBy !== "" && sted.By === valgtBy;
+    const passerVibe = valgtVibe === "" || sted.Vibe?.includes(valgtVibe);
+    const passerMusikk = valgtMusikk === "" || sted.Musikk?.includes(valgtMusikk);
+    return passerBy && passerVibe && passerMusikk;
   });
 
   function nullstill() {
